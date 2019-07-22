6bb06447d17888995586e77a86ea01575daace7a
Soumitra Roy Sarkar <projwebinfo@gmail.com>
2019-07-22 13:19:00 +0530

    Add files via upload

---
+++ junit-jacoco/src/main/java/com/jeejava/junit/jacoco/App.java
@@ -0,0 +1,13 @@
+package com.jeejava.junit.jacoco;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
